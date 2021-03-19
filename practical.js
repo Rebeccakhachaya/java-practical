8cb316fc72c28882b36643302c4ef9a7b0bb3e48
rebeccakhachaya <rebeccakhachaya@gmail.com>
2021-03-19 21:10:21 +0300

    work

---
+++ practical.js
@@ -0,0 +1,13 @@
+function name(){
+    var a="becky";
+    var b="pamela";
+    function identity(){
+        var c= a + " and " + b + " are friends ";
+        console.log(c)
+    }
+    identity();
+}
+name();
+
+
+    
