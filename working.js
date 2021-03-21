dbdcd5a0ed557048896d0f14cbe9ed525500243e
rebeccakhachaya <rebeccakhachaya@gmail.com>
2021-03-21 12:54:40 +0300

    work

---
+++ working.js
@@ -0,0 +1,13 @@
+var fruit={
+    name:"orange",
+    price:30,
+    quantity:2,
+    fruitCost:function(price,quantity){
+        totalCost=price*quantity;
+        return fruitCost=totalCost;
+
+    }
+   
+}
+fruit.fruitCost(30,2);
+console.log(  fruit.quantity  + " " + fruit.name + " for KES " + totalCost);
